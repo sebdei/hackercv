c7476d551ddbc273ffb3adf94ee9d8e44b945be5
Nicho Kappel <you@example.com>
2021-03-28 00:41:49 +0100

    footer misc

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,28 @@
 <template>
-  <!-- Footer -->
-  <footer class="text-center ">
-    <!-- Grid container -->
-    <div class="container p-4">
-      <FooterLinks :links="links"/>
+  <footer class="text-center container rounded">
+
+    <div v-if="links.length > 0 ">
+      <div class="container py-3">
+        <FooterLinks :links="links"/>
+      </div>
     </div>
 
-    <!-- Copyright -->
-    <div class="text-center p-3">
+    <!-- Theme -->
+    <div class="text-center py-3">
       Theme created with
       <a href="https://github.com/sebdei/hackercv">hackercv</a>
       by Seb &#38; Nicho.
     </div>
-    <!-- Copyright -->
+    <!-- Theme -->
+
+    <!-- Disclaimer -->
+    <div class="container pb-3">
+      <a href="disclaimer" class="mx-1 disclaimer">Disclaimer</a>
+      <a href="gdpr" class="mx-1 disclaimer">GDPR</a>
+    </div>
+    <!-- Disclaimer -->
 
   </footer>
-  <!-- Footer -->
 </template>
 <script>
 import FooterLinks from './FooterLinks'
@@ -37,5 +44,10 @@ export default {
   footer {
     background-color: #454545;
     color: #eeeeee;
+  }
+
+  .disclaimer:link {
+    color: #888888;
+    font-size: 0.7em;
   }
 </style>
--- src/components/FooterLinks.vue
+++ src/components/FooterLinks.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    Find me:
     <span v-for="item in links" :key="item.link" class="mx-2">
       <a href="{{ item.link }}"><span v-html="item.text"/></a>
     </span>
