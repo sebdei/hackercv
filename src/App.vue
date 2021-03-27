1ccab34462079500e09b73bf59a609f7a1f30f47
Nicho Kappel <you@example.com>
2021-03-27 22:31:31 +0100

    initial footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <Header/>
-
     <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- Footer -->
+  <footer class="text-center ">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a class="btn btn-primary m-1" href="#!" role="button">Barfoo</a>
+
+        <!-- Twitter -->
+        <a class="btn btn-primary btn-floating m-1" href="#!" role="button">Foobar</a>
+
+        <!-- Google -->
+        <a class="btn btn-primary btn-floating m-1" href="#!" role="button">dwdwdwd</a>
+      </section>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3">
+      Theme created with
+      <a href="https://github.com/sebdei/hackercv">hackercv</a>
+      by Seb &#38; Nicho.
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+  footer {
+    background-color: #454545;
+    color: #eeeeee;
+  }
+</style>
