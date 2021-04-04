84dd1a3c4c73ed01002639bae6b113a8bab76066
sebdei <seb.deisel@posteo.de>
2021-04-04 21:58:03 +0200

    grid component

---
+++ src/components/layout/GridContent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid">
+    <div :key="item.id" v-for="item in items">
+      <slot v-bind:item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+}
+</style>
