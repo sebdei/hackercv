56a9d393fb49bc294006b2c02e47ccce87aaac82
sebdei <seb.deisel@posteo.de>
2021-04-09 22:50:00 +0200

    added dynamic component  load

---
+++ src/views/blog/BlogDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="blogpostComponent" v-if="blogpostComponent"/>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+import NotFound404 from '@/components/error/NotFound404'
+
+export default {
+  computed:  {
+    blogpostComponent: function () {
+      const fileName = this.$route.params.fileName
+      return defineAsyncComponent({
+        loader: () => import(`@/ressources/views/blog/blogposts/${fileName}`),
+        errorComponent: NotFound404
+      })
+    }
+  }
+}
+</script>
