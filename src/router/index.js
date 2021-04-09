23d9b0a596f7a8b226a60facfb41c22f0f4cf907
sebdei <seb.deisel@posteo.de>
2021-04-09 22:49:40 +0200

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,29 +2,22 @@ import { createWebHistory, createRouter } from "vue-router"
 
 import About from "@/views/about/About"
 import Blog from "@/views/blog/Blog"
+import BlogDetails from "@/views/blog/BlogDetails"
+
 import Projects from "@/views/projects/Projects"
 
 const routes = [
-  {
-    path: "/",
-    name: "About",
-    component: About
-  },
-  {
-    path: "/blog",
-    name: "Blog",
-    component: Blog
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    component: Projects
-  }
-];
+  { path: "/", name: "About", component: About },
+
+  { path: "/blog", name: "Blog", component: Blog },
+  { path: "/blog/:fileName", name: "BlogDetails", component: BlogDetails },
+
+  { path: "/projects", name: "Projects", component: Projects }
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
 export default router
