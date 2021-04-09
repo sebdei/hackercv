76782f22fea36d0e32c78d5a2243a8df2ccc6794
sebdei <seb.deisel@posteo.de>
2021-04-09 22:49:49 +0200

    blog view update with router l ink

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -2,12 +2,14 @@
   <View title="Blog" :subTitle="content.subTitle">
     <GridContent :items="content.articles">
       <template v-slot:default="itemScope">
-        <Card
-          class="d-flex h-100"
-          :sub-title="itemScope.item.subTitle"
-          :title="itemScope.item.title">
-          {{ itemScope.item.content }}
-        </Card>
+        <router-link :to="{ name: 'BlogDetails', params: { fileName: itemScope.item.fileName }}">
+          <Card
+            class="d-flex h-100"
+            :sub-title="itemScope.item.subTitle"
+            :title="itemScope.item.title">
+            {{ itemScope.item.content }}
+          </Card>
+        </router-link>
       </template>
     </GridContent>
   </View>
@@ -17,7 +19,7 @@
 import content from '@/ressources/views/blog/content.json'
 
 import Card from '@/components/card/Card'
-import GridContent  from '@/components/layout/GridContent'
+import GridContent from '@/components/layout/GridContent'
 import View from '@/components/layout/View'
 
 export default {
