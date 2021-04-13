3e6391ce84a42a962ac3c6eb2963975b1c538ca7
sebdei <seb.deisel@posteo.de>
2021-04-13 23:21:43 +0200

    home comp

---
+++ src/views/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="row d-flex justify-content-between">
+      <div class="col-12 col-md-7 col-lg-6">
+        <h2 class="mb-4">{{ content.title }}</h2>
+        <p>
+          {{ content.subTitle }}
+        </p>
+      </div>
+      <div class="col-10 col-md-5 col-lg-4">
+        <div class="d-flex justify-content-end">
+          <img class="img-fluid" :src="content.avatarUrl" alt="hacker" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import content from '@/ressources/views/home/content.json'
+
+export default {
+  computed: {
+    content: function () {
+      return content
+    }
+  }
+}
+</script>
+
+<style scoped>
+.persona {
+  height: 300px;
+  width: 300px;
+}
+</style>
