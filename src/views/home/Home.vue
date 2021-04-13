807554b5f60ab16697ce18f895e041be0e8be4be
sebdei <seb.deisel@posteo.de>
2021-04-13 23:43:06 +0200

    removed redundant div

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <div class="row d-flex justify-content-between">
-      <div class="col-12 col-md-7 col-lg-6">
-        <h2 class="mb-4">{{ content.title }}</h2>
-        <p>
-          {{ content.subTitle }}
-        </p>
-      </div>
-      <div class="col-12 col-md-5 col-lg-3">
-        <img class="img-fluid" :src="content.avatarUrl" alt="hacker" />
-      </div>
+  <div class="row d-flex justify-content-between">
+    <div class="col-12 col-md-7 col-lg-6">
+      <h2 class="mb-4">{{ content.title }}</h2>
+      <p>
+        {{ content.subTitle }}
+      </p>
+    </div>
+    <div class="col-12 col-md-5 col-lg-3">
+      <img class="img-fluid" :src="content.avatarUrl" alt="hacker" />
     </div>
   </div>
 </template>
