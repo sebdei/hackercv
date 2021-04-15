51f8c7bfbceda533e612d9be2f7b336bfa6cc1b1
sebdei <seb.deisel@posteo.de>
2021-04-15 12:44:50 +0200

    added cta to home and about

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="row d-flex justify-content-between">
-    <div class="col-12 col-md-7 col-lg-6">
-      <h2 class="mb-4">
-        {{ content.title }}
-      </h2>
-      <p>
-        {{ content.subTitle }}
-      </p>
+  <div>
+    <div class="row d-flex justify-content-between">
+      <div class="col-12 col-md-7 col-lg-6">
+        <h2 class="mb-4">
+          {{ content.title }}
+        </h2>
+        <p>
+          {{ content.subTitle }}
+        </p>
+      </div>
+      <div class="col-12 col-md-5 col-lg-3">
+        <img class="img-fluid rounded" :src="content.avatarUrl" alt="hacker" />
+      </div>
     </div>
-    <div class="col-12 col-md-5 col-lg-3">
-      <img class="img-fluid rounded" :src="content.avatarUrl" alt="hacker" />
-    </div>
+
+    <CtaLink :title="content.ctaTitle" :to="{ name: 'About' }" />
   </div>
 </template>
 
 <script>
 import content from '@/ressources/views/home/content.json'
 
+import CtaLink from '@/components/buttons/CtaLink'
+
 export default {
+  components: {
+    CtaLink
+  },
   computed: {
     content: function () {
       return content
