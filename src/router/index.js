a2b7fd5467f7fd368040a6aaabde73dd583b4b06
sebdei <seb.deisel@posteo.de>
2021-04-25 20:42:40 +0200

    added imprint

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import About from "@/views/about/About"
 import Blog from "@/views/blog/Blog"
 import BlogDetails from "@/views/blog/BlogDetails"
 import Home from '@/views/home/Home'
+import Imprint from '@/views/imprint/Imprint'
 import Projects from "@/views/projects/Projects"
 
 const routes = [
@@ -14,6 +15,8 @@ const routes = [
   { path: "/blog", name: "Blog", component: Blog },
   { path: "/blog/:fileName", name: "BlogDetails", component: BlogDetails },
 
+  { path: "/imprint", name: "Imprint", component: Imprint },
+  
   { path: "/projects", name: "Projects", component: Projects }
 ]
 
