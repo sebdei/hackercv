e6af91ec3dbb163df073c86c245867e433d2005f
Sebastian Deisel <sebastian.deisel@loyjoy.com>
2022-11-06 12:30:37 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router"
 
 import About from "@/views/about/About"
-import Blog from "@/views/blog/Blog"
-import BlogDetails from "@/views/blog/BlogDetails"
 import Home from '@/views/home/Home'
 import Imprint from '@/views/imprint/Imprint'
 import Projects from "@/views/projects/Projects"
@@ -11,12 +9,7 @@ const routes = [
   { path: "/", name: "Home", component: Home },
 
   { path: "/about", name: "About", component: About },
-
-  { path: "/blog", name: "Blog", component: Blog },
-  { path: "/blog/:fileName", name: "BlogDetails", component: BlogDetails },
-
   { path: "/imprint", name: "Imprint", component: Imprint },
-  
   { path: "/projects", name: "Projects", component: Projects }
 ]
 
