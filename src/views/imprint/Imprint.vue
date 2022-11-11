aed336e6f4c2803c2a72f79a4a2104540e5b7edb
Sebastian Deisel <sebastian.deisel@loyjoy.com>
2022-11-11 13:27:30 +0100

    Update Imprint.vue

--- src/views/imprint/Imprint.vue
+++ src/views/imprint/Imprint.vue
@@ -1,15 +1,8 @@
 <template>
   <View title="Impressum">
-    Angaben gemäß § 5 TMG
-    <br />
-    <br />
-
     Sebastian Deisel <br />
-
-    Martin-Luther-Straße 11 <br />
-    48147 Münster <br />
     <br />
-    E-Mail: sebastian.deisel@gmail.com
+    Contact me: sebastian.deisel@gmail.com
   </View>
 </template>
 
